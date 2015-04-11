1913ea48fd83694eca07eaeb6a4a6c5690ef4cfb
Gabrielle Tranchet <g.tranchet@gmail.com>
2015-04-11 13:46:35 +0200

    creates a second activity that receives the word of the first one

--- app/src/main/java/com/vocabworld/danigabi/vocabworld/SearchResultActivity.java
+++ app/src/main/java/com/vocabworld/danigabi/vocabworld/SearchResultActivity.java
@@ -5,33 +5,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SearchResultActivity extends ActionBarActivity {
 
-    public final static String EXTRA_WORD = "com.vocabworld.danigabi.vocabworld.WORD";
 
-    public void searchWord(View view){
-        Intent intent = new Intent(this,SearchResultActivity.class);
-        EditText editText = (EditText)findViewById(R.id.search_word);
-        String word = editText.getText().toString();
-        intent.putExtra(EXTRA_WORD, word);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_search_result);
+
+        Intent intent = getIntent();
+        String word = intent.getStringExtra(MainActivity.EXTRA_WORD);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText("No result found for \"" + word + "\"");
+
+        setContentView(textView);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_search_result, menu);
         return true;
     }
 
